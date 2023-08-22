5b137c3771fb6850cb51c61e7f0fc2e2ec42dd25
Carlos Lima <duca_liima@icloud.com>
2023-08-21 21:09:12 -0300

    add eslint config

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function (request, response) {
+app.get("/", (request, response) => {
   response.send("Hello World");
 });
 
-app.listen(3000, () =>
-  console.log("🔥 server listening on port http://localhost:3000")
-);
+app.listen(3000, () => {
+  console.log("🔥 server listening on port http://localhost:3000");
+});
