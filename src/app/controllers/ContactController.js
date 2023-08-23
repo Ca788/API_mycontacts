67bbf79de392d1f661af17c234604eb8480be7d6
Carlos Lima <duca_liima@icloud.com>
2023-08-23 20:02:08 -0300

    Show and delete routes created

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -7,8 +7,16 @@ class ContactController {
     response.json(contacts);
   }
 
-  show() {
-    //Obter um registros
+  async show(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(400).json({ error: "Not Found" });
+    }
+
+    response.json(contact);
   }
 
   store() {
@@ -19,8 +27,18 @@ class ContactController {
     //Editar um registro
   }
 
-  delete() {
-    //Deletear um registro
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: "Not Found" });
+    }
+
+    await ContactsRepository.delete(id);
+
+    response.sendStatus(204);
   }
 }
 
