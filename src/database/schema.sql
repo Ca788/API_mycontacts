dc9a5eb944b2885076514e248ad8f242cee3a27b
Carlos Lima <duca_liima@icloud.com>
2023-08-29 22:48:22 -0300

    Created contact tables and node connection to postgresql.

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -10,3 +10,14 @@ IF
       id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4()
       , name VARCHAR NOT NULL
     );
+
+      CREATE TABLE
+  IF
+    NOT EXISTS contacts (
+      id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4()
+      , name VARCHAR NOT NULL,
+      email VARCHAR UNIQUE,
+      Phone VARCHAR,
+      Category_id UUID,
+      FOREIGN KEY(Category_id) REFERENCES categories(id)
+    );
